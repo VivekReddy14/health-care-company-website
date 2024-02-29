ca9ddc8fae5ab9634aca3c994da5ac3ea678cc41
VivekReddy14 <crdvrd@gmail.com>
2024-03-01 02:03:56 +0530

    initiaal commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from 'src/components/home-page/home-page.component';
+const routes: Routes = [
+  // { path: '', redirectTo: 'invoices', pathMatch: 'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent },
+  // { path: 'newinvoice', component: NewinvoiceComponent },
+  // { path: 'refunds', component: RefundComponent }
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from 'src/components/home-page/home-page.component';
+import { MainNavbarComponent } from 'src/components/main-navbar/main-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MainNavbarComponent
   ],
   imports: [
     BrowserModule,
